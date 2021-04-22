65d59ecf50d1a713505b094ab3426d65d44891cb
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-04-22 23:33:05 +0300

    Updates task-01

---
+++ js/task-01.js
@@ -0,0 +1,26 @@
+// Напиши скрипт, который выполнит следующие операции.
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. 
+    // Получится 'В списке 3 категории.'.
+
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка 
+    // элемента(тега h2) и количество элементов в категории(всех вложенных в него элементов li).
+
+// Например для первой категории получится:
+
+// Категория: Животные
+// Количество элементов: 4
+
+const categories = document.querySelector('ul#categories');
+const categoriesAmount = categories.children.length;
+const categoriesAmountMessage = `В списке ${categoriesAmount} категории.`;
+console.log(categoriesAmountMessage);
+
+const categoriesElem = document.querySelectorAll('.item');
+
+categoriesElem.forEach(elem => {
+    const categoriesElemTitle = elem.firstElementChild.textContent;
+    console.log(`Категория: ${categoriesElemTitle}`);
+    const categoriesElemAmount = elem.lastElementChild.children.length;
+    console.log(`Количество элементов: ${categoriesElemAmount}`);
+});
